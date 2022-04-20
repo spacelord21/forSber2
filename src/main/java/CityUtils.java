800ad62f8a9fc3d823c21879ef826b50a869b347
spacelord_21 <incrediblespaceee@gmail.com>
2022-04-20 17:22:16 +0200

    update for second task

---
+++ src/main/java/CityUtils.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class CityUtils {
+
+    public static List<City> parse() {
+        List<City> cities = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new File("src/main/resources/cities.csv"));
+            while (scanner.hasNextLine()) {
+                cities.add(parse(scanner.nextLine()));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+
+
+    public static void print(List<City> cities) {
+        cities.forEach(System.out::println);
+    }
+
+    private static City parse(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(";");
+        scanner.skip("\\d*");
+        String name = scanner.next();
+        String region = scanner.next();
+        String district = scanner.next();
+        int population = scanner.nextInt();
+        String foundation = null;
+        if (scanner.hasNext()) {
+            foundation = scanner.next();
+        }
+        scanner.close();
+        return new City(name, region, district, population, foundation);
+    }
+
+    public static List<City> sortAsAlphabetically() {
+        List<City> cities = parse();
+        cities.sort(Comparator.comparing(City::getName));
+        return cities;
+    }
+
+    public static List<City> doubleSortAsAlphabetically() {
+        List<City> cities = parse();
+        cities.sort(Comparator
+                .comparing(City::getDistrict)
+                .thenComparing(City::getName));
+        return cities;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<City> cities = new ArrayList<>();
-        try {
-            Scanner scanner = new Scanner(new File("src/main/resources/cities.csv"));
-            scanner.useDelimiter(";");
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] fields = line.split(";");
-                if(fields.length==6) {
-                    cities.add(new City(fields[1],fields[2],fields[3],Integer.parseInt(fields[4]),fields[5]));
-                }
-                else{
-                    cities.add(new City(fields[1],fields[2],fields[3],Integer.parseInt(fields[4]),""));
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        for(City city : cities) {
-            System.out.println(city.toString());
-        }
+        CityUtils.print(CityUtils.sortAsAlphabetically());
+        CityUtils.print(CityUtils.doubleSortAsAlphabetically());
     }
 }
